ecad5aecd305e34b2e5580db4540b1beddfcc2d2
unknown <winonnarivero@gmail.com>
2023-03-30 22:10:14 +0800

    added functions

--- cypress/e2e/flender.cy.js
+++ cypress/e2e/flender.cy.js
@@ -4,6 +4,7 @@ describe('Create Protocol;', () => {
   const customTimeOut = {timeout: 3000000}
 
   beforeEach(() => {
+    cy.wait(5000)
     cy.visit('/', customTimeOut)
     cy.get('input#username', customTimeOut).type('t-winonna').should('have.value', 't-winonna')
     cy.get('input#password', customTimeOut).type('123')
@@ -72,35 +73,29 @@ describe('Create Protocol;', () => {
 
 it.only('Container', () => {
 
-  cy.wait(3000)
- // cy.visit('https://flender.beta.reportheld.com/office/main.html#protocols', customTimeOut)
- cy.get('button.btn.btn-large.btn-block.btn-primary[type="button"]')
+  cy.wait(5000)
+  cy.get('button.btn.btn-large.btn-block.btn-primary[type="button"]', customTimeOut)
     .contains('Ok').should('be.visible').click()
-    cy.get('span[title="Protocols"]', customTimeOut).click()
-    cy.get('.groupxs-btn.btn-outline.btn-default').should('be.visible') 
-    .eq(0).click()
-  //   cy.get('select-powerplant-popup > base-popup > [data-bind="visible: showPopup, closeOnClick: showPopup"] > .popup > .wrapper > .alternative-contacts > .container-fluid > :nth-child(1) > .left > .btn-group > .btn > .filter-option').click()
     cy.wait(3000)
-    cy.get('.btn-group.bootstrap-select.select.open')
-    .find('.dropdown-menu.inner')
-    .should('be.visible')
-    .find('li')
-    .contains('Voerde')
-    .click();
-  
+    cy.get('span[title="Protocols"]', customTimeOut).click()
+    cy.get('.groupxs-btn.btn-outline.btn-default', customTimeOut).should('be.visible') 
+    .eq(0).click()
 
+  cy.get('button-container.left').first().within(() => {
+  cy.get('.button').click()
+  cy.contains('FLENDER').click()
 
-  // cy.get('button.btn.dropdown-toggle.btn-primary') 
-  // .eq(2).click() 
-  // .get('.dropdown-menu.open') 
-  // .should('be.visible') 
-  // .contains('FLENDER') 
-  // .click(); 
+  })
 
+  cy.wait(5000)
+  cy.get('button-container.right').first().within(() => {
+  cy.get('.button').click()
+  cy.contains('Voerde').click()
 
 })
-it.only('Container', () => {
+it.only('xx ', () => {
 
   cy.wait(3000)
+})
 })
 })
